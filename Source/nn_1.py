db6de29c2d2693768269b441fd47666a2c4be144
-dreamIIx <shiba.zheka65@gmail.com>
2023-02-25 18:30:36 +0700

    upd nn_1

--- Source/nn_1.py
+++ Source/nn_1.py
@@ -1,32 +1,86 @@
 import cv2
 import numpy as np
 
-image_file = "./0.png"
-img = cv2.imread(image_file)
-img = cv2.bitwise_not(img)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
-img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+from tensorflow import keras
+from keras.models import Sequential
+from keras import optimizers
+from keras.layers import Convolution2D, MaxPooling2D, Dropout, Flatten, Dense, Reshape, LSTM, BatchNormalization
+from keras.optimizers import SGD, RMSprop, Adam
+from keras import backend as K
+from keras.constraints import maxnorm
+import tensorflow as tf
 
-# Get contours
-contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+def letters_extract(image_file: str, out_size=32):
+    img = cv2.imread(image_file)
+    img = cv2.bitwise_not(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+    img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
 
-output = img.copy()
+    # Get contours
+    contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-for idx, contour in enumerate(contours):
-    (x, y, w, h) = cv2.boundingRect(contour)
-    # print("R", idx, x, y, w, h, cv2.contourArea(contour), hierarchy[0][idx])
-    # hierarchy[i][0]: the index of the next contour of the same level
-    # hierarchy[i][1]: the index of the previous contour of the same level
-    # hierarchy[i][2]: the index of the first child
-    # hierarchy[i][3]: the index of the parent
-    cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+    output = img.copy()
 
+    letters = []
+    for idx, contour in enumerate(contours):
+        (x, y, w, h) = cv2.boundingRect(contour)
+        # print("R", idx, x, y, w, h, cv2.contourArea(contour), hierarchy[0][idx])
+        # hierarchy[i][0]: the index of the next contour of the same level
+        # hierarchy[i][1]: the index of the previous contour of the same level
+        # hierarchy[i][2]: the index of the first child
+        # hierarchy[i][3]: the index of the parent
+        #if hierarchy[0][idx][3] == 0:
+        cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+        letter_crop = gray[y:y + h, x:x + w]
+        # print(letter_crop.shape)
 
-img = cv2.bitwise_not(img)
-output = cv2.bitwise_not(output)
-cv2.imshow("Input", img)
-cv2.imshow("Enlarged", img_erode)
-cv2.imshow("Output", output)
-cv2.drawContours(img, contours, -1, (0,255,0), 3)
-cv2.waitKey(0)
+        # Resize letter canvas to square
+        size_max = max(w, h)
+        letter_square = 255 * np.ones(shape=[size_max, size_max], dtype=np.uint8)
+        if w > h:
+            # Enlarge image top-bottom
+            # ------
+            # ======
+            # ------
+            y_pos = size_max//2 - h//2
+            letter_square[y_pos:y_pos + h, 0:w] = letter_crop
+        elif w < h:
+            # Enlarge image left-right
+            # --||--
+            x_pos = size_max//2 - w//2
+            letter_square[0:h, x_pos:x_pos + w] = letter_crop
+        else:
+            letter_square = letter_crop
+
+        # Resize letter to 28x28 and add letter and its X-coordinate
+        letters.append((x, w, cv2.resize(cv2.bitwise_not(letter_square), (out_size, out_size), interpolation=cv2.INTER_AREA)))
+
+    # Sort array in place by X-coordinate
+    letters.sort(key=lambda x: x[0], reverse=False)
+
+    return letters
+
+letters = letters_extract("./0.png")
+
+# i = 1
+# for letter in letters:
+#     cv2.imshow("test" + str(i), letter[2])
+#     i += 1
+# cv2.waitKey(0)
+
+token_labels = []
+
+def create_model():
+    model = Sequential()
+    model.add(Convolution2D(filters=32, kernel_size=(3, 3), padding='valid', input_shape=(32, 32, 1), activation='relu'))
+    model.add(Convolution2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(token_labels), activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+    return model
+
