33b0beace99133b26d0787398cc31b88f81c1103
-dreamIIx <shiba.zheka65@gmail.com>
2023-02-25 17:23:24 +0700

    upd nn_1

--- Source/nn_1.py
+++ Source/nn_1.py
@@ -3,12 +3,13 @@ import numpy as np
 
 image_file = "./0.png"
 img = cv2.imread(image_file)
+img = cv2.bitwise_not(img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
-#img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
+ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+img_erode = cv2.erode(thresh, np.ones((3, 3), np.uint8), iterations=1)
 
 # Get contours
-contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(img_erode, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 output = img.copy()
 
@@ -22,8 +23,10 @@ for idx, contour in enumerate(contours):
     cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
 
 
+img = cv2.bitwise_not(img)
+output = cv2.bitwise_not(output)
 cv2.imshow("Input", img)
-cv2.imshow("Enlarged", gray)
+cv2.imshow("Enlarged", img_erode)
 cv2.imshow("Output", output)
 cv2.drawContours(img, contours, -1, (0,255,0), 3)
 cv2.waitKey(0)
