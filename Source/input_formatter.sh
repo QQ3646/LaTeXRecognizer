0b81a46f6be2bc87fcf9cf735decbf8e85a45856
-dreamIIx <shiba.zheka65@gmail.com>
2023-02-25 16:22:16 +0700

    create formatted input

--- Source/input_formatter.sh
+++ Source/input_formatter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for f in $(find ../input/ -name '*.jpg');
+for f in $(find ../input/ -name '*.png');
 do
 echo "Processing $f"
 file=${f##*/}
 echo "a.k.a. $file"
 dirpath=${f%/*/*}
 mkdir -p ${dirpath}/formatted_input
-ffmpeg -i "$f" -vf "[in] scale='min(515,iw)':-1:force_original_aspect_ratio=decrease,pad=515:515:-1:-1:color=white, colorkey=white [out]" "${dirpath}/formatted_input/${file%.jpg}.png"
+ffmpeg -i "$f" -vf "[in] scale='min(515,iw)':-1:force_original_aspect_ratio=decrease,pad=515:515:-1:-1:color=white, colorkey=white [out]" "${dirpath}/formatted_input/${file%.png}.png"
 done
